a80f90630e50d4f4202479c2d1f24ed8900c1717
wspereira <splitpereira@gmail.com>
2022-06-21 19:44:09 -0500

    Add custom circuitbreak

--- item-service/src/main/java/com/wspereira/udemy/microservice/itemservice/config/Appconfig.java
+++ item-service/src/main/java/com/wspereira/udemy/microservice/itemservice/config/Appconfig.java
@@ -1,9 +1,16 @@
 package com.wspereira.udemy.microservice.itemservice.config;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import java.time.Duration;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
 @Configuration
 public class Appconfig {
 
@@ -11,5 +18,24 @@ public class Appconfig {
     @LoadBalanced
     public RestTemplate registerRestTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory
+                -> factory.configureDefault(
+                        id -> {
+                            return new Resilience4JConfigBuilder(id)
+                                    .circuitBreakerConfig(CircuitBreakerConfig.custom()
+                                            .slidingWindowSize(10)
+                                            .failureRateThreshold(50)
+                                            .waitDurationInOpenState(Duration.ofSeconds(10L))
+                                            .permittedNumberOfCallsInHalfOpenState(5)
+                                            .build())
+                                    .timeLimiterConfig(TimeLimiterConfig.ofDefaults())
+                                    .build();
+
+                        });
+
     }
 }
