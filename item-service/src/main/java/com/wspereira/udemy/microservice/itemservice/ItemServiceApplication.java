c571acf4afe9c6de275a052b0306cff9ab2119d5
wspereira <splitpereira@gmail.com>
2022-06-22 10:59:09 -0500

    Agregando profile ambientes

--- item-service/src/main/java/com/wspereira/udemy/microservice/itemservice/ItemServiceApplication.java
+++ item-service/src/main/java/com/wspereira/udemy/microservice/itemservice/ItemServiceApplication.java
@@ -4,18 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
-@PropertySources({
-    @PropertySource(value = "file:config/application.yml")})
+
 public class ItemServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ItemServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ItemServiceApplication.class, args);
+    }
 
 }
